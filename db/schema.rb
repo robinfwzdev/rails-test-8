e2e8413f290dbe36d91d86383860b16d28c861cf
robinfwzdev <robin@futureworkz.com>
2016-03-17 01:18:36 +0700

    Remove fields on users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316162735) do
+ActiveRecord::Schema.define(version: 20160316181502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,6 @@ ActiveRecord::Schema.define(version: 20160316162735) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "name"
   end
 
