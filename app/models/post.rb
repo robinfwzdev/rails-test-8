e196ba3e23621724aac9cbb18cf78675e577b62d
robinfwzdev <robin@futureworkz.com>
2016-03-17 02:21:33 +0700

    Add association from post to comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, :content,  presence: true
 
   belongs_to :user
+  has_many   :comments, dependent: :destroy
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Post, type: :model do
 
   context 'Association' do
     it { is_expected.to belong_to :user }
+    it { is_expected.to have_many :comments }
   end
 end
