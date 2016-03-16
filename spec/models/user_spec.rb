964b28d431be654a5d991dbd73101a4b89de6196
robinfwzdev <robin@futureworkz.com>
2016-03-17 02:22:34 +0700

    Add association from user to comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'Association' do
     it { is_expected.to have_many :posts }
+    it { is_expected.to have_many :comments }
   end
 end
