6887fa20abc503dc481dbc0345b7dc5a15048eab
robinfwzdev <robin@futureworkz.com>
2016-03-17 02:00:10 +0700

    Add association from posts to user

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context 'Validation' do
-    it { should validate_presence_of :title }
-    it { should validate_presence_of :content }
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :content }
+  end
+
+  context 'Association' do
+    it { is_expected.to belong_to :user }
   end
 end
