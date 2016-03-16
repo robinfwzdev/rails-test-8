e605956bd5722999834f88cfb4f111e73a91c9a0
robinfwzdev <robin@futureworkz.com>
2016-03-17 02:19:18 +0700

    Add association from comments to user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   validates :message,  presence: true
+
+  belongs_to :user
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Comment, type: :model do
   context 'Validation' do
     it { is_expected.to validate_presence_of :message }
   end
+
+  context 'Association' do
+    it { is_expected.to belong_to :user }
+  end
 end
