8427ff385d5cf66364cd898d523ae735ed23c29f
robinfwzdev <robin@futureworkz.com>
2016-03-20 10:06:09 +0700

    Add data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 User.destroy_all
+Post.destroy_all
 
-User.create!(name: "robin hood", email: "robin@example.com", password: "123123123")
+user = User.create!(name: "robin hood", email: "robin@example.com", password: "123123123")
+Post.create!(title: "Practice ruby", content: "Read a lot of code", user_id: user.id)
