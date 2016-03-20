4e3b17be3bd8555cf02c4f6550f737ef3147edff
robinfwzdev <robin@futureworkz.com>
2016-03-20 21:23:07 +0700

    Add belong_to user_profile on comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Comment, type: :model do
   context 'Association' do
     it { is_expected.to belong_to :user }
     it { is_expected.to belong_to :post }
+    it { is_expected.to belong_to :user_profile }
   end
 end
