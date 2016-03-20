45cf8400f4f23c72e22fd9b22c00eba9561b62ac
robinfwzdev <robin@futureworkz.com>
2016-03-20 21:41:48 +0700

    Create user_profile_comment action on UserProfilesController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,9 +11,23 @@ class CommentsController < ApplicationController
     end
   end
 
+  def user_profile_comment
+    @user_profile    = UserProfile.find(user_profile_id)
+    @comment = @user_profile.comments.new(message: params[:message], user_id: current_user.id )
+    @comment.save
+
+    respond_to do |format|  
+      format.js { render template: "user_profiles/show.js.erb" }
+    end
+  end
+
   private
 
   def post_id
     params.require(:post_id)
+  end
+
+  def user_profile_id
+    params.require(:user_profile_id)
   end
 end
