c52212df783d41524f2282b83b9406a444acd5fc
robinfwzdev <robin@futureworkz.com>
2016-03-20 22:07:31 +0700

    Write testcase for show action on PostsController

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  include Devise::TestHelpers
+
   describe '#show' do
     let!(:user) { create(:user) }
     let!(:post) { create(:post, user_id: user) }
+
+    before { sign_in user }
 
     it 'show post details' do
       get :show, id: post.id
