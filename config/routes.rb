8f9f7f1f379ba3fe5eb315ba5e3924f274c521d4
robinfwzdev <robin@futureworkz.com>
2016-03-20 10:03:27 +0700

    Create post_comment action for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: :show
-  resources :posts, only: :show
+  resources :posts, only: :show do
+    resources :comments do
+      collection do
+        get 'post_comment'
+      end
+    end
+  end
 end
