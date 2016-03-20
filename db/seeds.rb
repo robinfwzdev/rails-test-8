0f6db3d5287441268235829bea663cedb5adfb3e
robinfwzdev <robin@futureworkz.com>
2016-03-20 22:08:33 +0700

    Add avatar for user_profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ Post.destroy_all
 UserProfile.destroy_all
 
 user = User.create!(name: "robin hood", email: "robin@example.com", password: "123123123")
-UserProfile.create!(name: "test test", email: "test@example.com", password: "123123123")
+UserProfile.create!(name: "test test", email: "test@example.com", password: "123123123", avatar: "images/avatar.png")
 Post.create!(title: "Practice ruby", content: "Read a lot of code", user_id: user.id)
