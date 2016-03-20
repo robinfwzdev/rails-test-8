9b71b056a5a7aee8f415579319bc0503db9fbb8b
robinfwzdev <robin@futureworkz.com>
2016-03-20 21:36:07 +0700

    Create data seed for UserProfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 User.destroy_all
 Post.destroy_all
+UserProfile.destroy_all
 
 user = User.create!(name: "robin hood", email: "robin@example.com", password: "123123123")
+UserProfile.create!(name: "test test", email: "test@example.com", password: "123123123")
 Post.create!(title: "Practice ruby", content: "Read a lot of code", user_id: user.id)
